3d218ef5ecf7395b44cf8af79300633ecd986eb1
Alexander Ryan <atryan@iastate.edu>
2024-04-03 10:47:03 -0500

    added basic methods

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {app} from './script.js';
+import {app, cart, confirm} from './script.js';
 
 
 
@@ -9,5 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <app />
+    <cart />
+    <confirm />
   </React.StrictMode>
 );
--- src/script.js
+++ src/script.js
@@ -3,3 +3,12 @@ import react, {useState} from "react";
 export function app(){
 
 }
+
+export function cart() {
+
+}
+
+export function confirm() {
+
+    
+}
